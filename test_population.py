0462e563db9c934d17441f5ecc6619bb9e395823
llunasv <vladaglov@gmail.com>
2026-04-10 15:57:11 +0300

    test: add parameterized tests for population change logic

--- test_population.py
+++ test_population.py
@@ -12,3 +12,17 @@ def test_calculate_change_structure(sample_data):
     results = calculate_population_change(sample_data)
     assert isinstance(results, dict)
     assert "Ukraine" in results
+
+@pytest.mark.parametrize("country, y1, p1, y2, p2, expected", [
+    ("Poland", 2020, 380, 2021, 381, 1),
+    ("France", 2010, 65, 2011, 64, -1),
+    ("Germany", 2022, 80, 2023, 80, 0),
+])
+def test_calculation_logic(country, y1, p1, y2, p2, expected):
+    data = [
+        {"country": country, "year": y1, "population": p1},
+        {"country": country, "year": y2, "population": p2}
+    ]
+    res = calculate_population_change(data)
+    year_range = f"{y1}-{y2}"
+    assert res[country][0][year_range] == expected
