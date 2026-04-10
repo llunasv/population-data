f13343022bca940205752899a8a1f7817db72c56
llunasv <vladaglov@gmail.com>
2026-04-10 15:37:51 +0300

    feat: add calculate_population_change function

--- population_analisys.py
+++ population_analisys.py
@@ -13,3 +13,22 @@ def read_population_data(file_path):
                     "population": int(population.strip())
                 })
     return data
+
+def calculate_population_change(data):
+    changes = {}
+    sorted_data = sorted(data, key=lambda x: (x['country'], x['year']))
+    
+    for i in range(1, len(sorted_data)):
+        curr = sorted_data[i]
+        prev = sorted_data[i-1]
+        
+        if curr['country'] == prev['country']:
+            country = curr['country']
+            year_range = f"{prev['year']}-{curr['year']}"
+            change = curr['population'] - prev['population']
+            
+            if country not in changes:
+                changes[country] = []
+            changes[country].append({year_range: change})
+            
+    return changes
